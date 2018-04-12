351f7701697af25ddcf957930dc5af4b3477c21e
Louis Magdaleno <buildthatapp@gmail.com>
2018-04-12 10:20:51 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var cardsFlipped = 0
 var card_values = [];
 var card_elements = [];
+var correct = 0;
+var incorrect = 0;
 
 $( document ).ready(function() {	
 	$('card').click(function() {
@@ -14,17 +16,60 @@ $( document ).ready(function() {
 			console.log(card_values);
 			console.log(card_elements);
 			if (cardsFlipped === 2) {
-			compare_cards();
+				compare_cards();
 		}}})});
 
 function compare_cards() {
-	if (card_values[0] === card_values[1]) {
+	if (card_values[0] === card_values[1])  {
 		$('#' + card_elements[0]).addClass('matchFound');
 		$('#' + card_elements[1]).addClass('matchFound');
-		
-	}
-};
+		setTimeout(
+			function() {
+				$('#' + card_elements[0]).removeClass('matchFound');
+				$('#' + card_elements[1]).removeClass('matchFound');
+				clear_cards_flipped();
+				empty_array(card_values);
+				empty_array(card_elements);
+				correct += 1;
+				if (correct === 8) {
+					if (confirm('Congratulations! You have won. Would you like to play again?')) {
+						restart_game();
+					}
+					else {
+						alert('Thanks for playing');
+					}
+				}
+			}, 750);
 
+	}
+	else {
+
+		$('#' + card_elements[0]).addClass('matchNotFound');
+		$('#' + card_elements[1]).addClass('matchNotFound');
+		
+		setTimeout(
+			function() {
+				$('#' + card_elements[0]).removeClass('matchNotFound').removeClass('revealCard');
+				$('#' + card_elements[1]).removeClass('matchNotFound').removeClass('revealCard');
+				clear_cards_flipped();
+				empty_array(card_values);
+				empty_array(card_elements);
+				incorrect += 1;
+			}, 750);
+	}
+}
+
+function empty_array(my_array) {
+	my_array.length = 0;
+}
+
+function clear_cards_flipped(cards) {
+	cardsFlipped = 0;
+}
+
+function restart_game() {
+	location.reload(true)
+}
 
 	
 	
