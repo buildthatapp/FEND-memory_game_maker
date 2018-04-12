1fede05dacfeb98ab3631253441a3b69cf1c57ee
Louis Magdaleno <buildthatapp@gmail.com>
2018-04-12 10:57:52 -0700

    code clean up
    cleaned up html, css, and js using https://www.10bestdesign.com/dirtymarkup/

--- js/main.js
+++ js/main.js
@@ -17,9 +17,9 @@ var game_win_condition = 8;
 var max_face_up_cards = 2;
 var wait_time = 750;
 
-$( document ).ready(function() {
+$(document).ready(function() {
 	//Begin click event handling for the card elements	
-	$('card').click(function() {
+	$('.project').click(function() {
 		//Does not pass when the user has flipped over 2 cards
 		if (cardsFlipped < max_face_up_cards) {
 			$(this).addClass('revealCard');
@@ -30,47 +30,47 @@ $( document ).ready(function() {
 			if (cardsFlipped === max_face_up_cards) {
 				//when the user has flipped over 2 cards, compare their values
 				compare_cards();
-		}}})});
+			}
+		}
+	})
+});
 //compares the value within two cards. accepts no params.
 function compare_cards() {
 	//if the value stored within the two flipped cards is the same, then add a matchFound class to the card element
-	if (card_values[0] === card_values[1])  {
+	if (card_values[0] === card_values[1]) {
 		$('#' + card_elements[0]).addClass('matchFound');
 		$('#' + card_elements[1]).addClass('matchFound');
 		//wait a bit, then remove the matchFound class BUT leave the cards face up. clear arrays to allow player to flip over 2 more cards
-		setTimeout(
-			function() {
-				$('#' + card_elements[0]).removeClass('matchFound');
-				$('#' + card_elements[1]).removeClass('matchFound');
-				clear_cards_flipped();
-				empty_array(card_values);
-				empty_array(card_elements);
-				correct += 1;
-				//if the user has correctly matched all pairs, then ask if they want to play again
-				if (correct === game_win_condition) {
-					if (confirm('Congratulations! You have won. Would you like to play again?')) {
-						restart_game();
-					}
-					else {
-						alert('Thanks for playing');
-					}
+		setTimeout(function() {
+			$('#' + card_elements[0]).removeClass('matchFound');
+			$('#' + card_elements[1]).removeClass('matchFound');
+			clear_cards_flipped();
+			empty_array(card_values);
+			empty_array(card_elements);
+			correct += 1;
+			//if the user has correctly matched all pairs, then ask if they want to play again
+			if (correct === game_win_condition) {
+				if (confirm(
+						'Congratulations! You have won. Would you like to play again?')) {
+					restart_game();
+				} else {
+					alert('Thanks for playing');
 				}
-			}, wait_time);
-
-	}
-	else {
+			}
+		}, wait_time);
+	} else {
 		$('#' + card_elements[0]).addClass('matchNotFound');
 		$('#' + card_elements[1]).addClass('matchNotFound');
-		
-		setTimeout(
-			function() {
-				$('#' + card_elements[0]).removeClass('matchNotFound').removeClass('revealCard');
-				$('#' + card_elements[1]).removeClass('matchNotFound').removeClass('revealCard');
-				clear_cards_flipped();
-				empty_array(card_values);
-				empty_array(card_elements);
-				incorrect += 1;
-			}, wait_time);
+		setTimeout(function() {
+			$('#' + card_elements[0]).removeClass('matchNotFound').removeClass(
+				'revealCard');
+			$('#' + card_elements[1]).removeClass('matchNotFound').removeClass(
+				'revealCard');
+			clear_cards_flipped();
+			empty_array(card_values);
+			empty_array(card_elements);
+			incorrect += 1;
+		}, wait_time);
 	}
 }
 
@@ -86,7 +86,3 @@ function clear_cards_flipped(cards) {
 function restart_game() {
 	location.reload(true)
 }
-
-	
-	
-
