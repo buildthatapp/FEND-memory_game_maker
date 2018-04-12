b97d7f39af9568b54469d88b30c92205ac9f8d77
Louis Magdaleno <buildthatapp@gmail.com>
2018-04-12 09:37:26 -0700

    update
    changed article containers to card containers in index.html
    added id's to each card container in index.html
    added js to flip cards over. only 2 cards may be flipped over at a time.
    added js to check if cards flipped over match. if they match, add a class to turn the card green and shake the card.
    TODO - add logic to leave those two cards flipped over and allow for two more cards to be flipped. also, need to set a timer for how long to leave cards flipped over if they do not match

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-$( document ).ready(function() {
-	$('article').click(function() {
-		$(this).addClass('revealCard');
-})});
+var cardsFlipped = 0
+var card_values = [];
+var card_elements = [];
+
+$( document ).ready(function() {	
+	$('card').click(function() {
+		if (cardsFlipped < 2) {
+			$(this).addClass('revealCard');
+			card_values.push($(this ).text());
+			card_elements.push($(this).attr('id'))
+			cardsFlipped = cardsFlipped + 1;
+			//printing values to check if array is storing values correctly
+			console.log(cardsFlipped);
+			console.log(card_values);
+			console.log(card_elements);
+			if (cardsFlipped === 2) {
+			compare_cards();
+		}}})});
+
+function compare_cards() {
+	if (card_values[0] === card_values[1]) {
+		$('#' + card_elements[0]).addClass('matchFound');
+		$('#' + card_elements[1]).addClass('matchFound');
+		
+	}
+};
+
+
+	
+	
 
